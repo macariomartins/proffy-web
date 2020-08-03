1dace1584595ac99a763c1746c19324987f68e94
Macário Martins <macariomartinsjunior@gmail.com>
2020-08-03 20:24:47 -0300

    Add initial style and assets to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './assets/styles/global.css';
 
 function App() {
   return (
-    <h1>Hello, world!</h1>
+    <>
+      <h1>Hello, world!</h1>
+    </>
   );
 }
 
