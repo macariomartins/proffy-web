c642846397f2d75488465fa836c6860758a5aaef
Macário Martins <macariomartinsjunior@gmail.com>
2020-08-03 21:50:17 -0300

    Create Teacher pages

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -24,19 +26,21 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Ensinar" />
             Ensinar
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+          Total de 200 conexões já realizadas
+          {' '}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
