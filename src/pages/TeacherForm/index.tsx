e99547d1c3231848f207bc4f2ff0228a6259b175
Macário Martins <macariomartinsjunior@gmail.com>
2020-08-05 12:41:11 -0300

    Add style to TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,12 +2,38 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+
 function TeacherForm() {
   return (
-    <PageHeader
-      title="Que incrível que você que ensinar."
-      description="O primeiro passo é preencher esse formulário de inscrição"
-    />
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você que ensinar."
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Nome completo</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">WhatsApp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+
+      </main>
+    </div>
   );
 }
 
